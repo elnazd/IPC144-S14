40b241e2e7e67b7f1b0a17464a1c96abd94bad7f
Catherine Leung <cathyatseneca@gmail.com>
2014-05-16 13:59:59 -0400

    coded last digit.c

--- secF/lastdigit.c
+++ secF/lastdigit.c
@@ -1,2 +1,13 @@
 /*This program will ask the user to enter a number.
   it will then print out what the last digit of that number is.*/
+#include <stdio.h>
+
+int main(void){
+	int number;
+	int lastDigit;
+	printf("please enter a number: ");
+	scanf("%d",&number);
+	lastDigit = number % 10;
+	printf("the last digit of %d is %d\n",number,lastDigit);
+	return 0;
+}
