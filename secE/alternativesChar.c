d5f97bf08870ab65c8a0b613f717ed59db18ed6f
elnazd <elnaz.delpisheh@gmail.com>
2014-05-20 23:40:41 -0400

    Create alternativesChar.c

---
+++ secE/alternativesChar.c
@@ -0,0 +1,30 @@
+/*Write a C program to prompt 
+the user to select between two options ‘a’ or ‘b’. 
+If option ‘a’ is selected, display “Program 1 is running!”. If option ‘b’ is selected, display “Program 2 is running!”. Here is a sample output:
+
+Which option will you choose: a
+Program 1 is running!*/
+#include<stdio.h>
+int main(){
+	
+	char option;
+	printf("Please enter the option:");
+	
+	scanf("%c",&option);
+	
+	switch (option){
+	
+	case 'a':
+		printf("Program 1 is running!");
+		break;
+	case 'b':
+		printf("Program 2 is running!");
+		break;
+	
+	default:
+		printf("Invalid input!");
+	}
+	
+	return 0;
+	
+}
