5c98a6880a365ecf90dbd89ea7b9cffb34065755
elnazd <elnaz.delpisheh@gmail.com>
2014-05-22 07:19:12 -0400

    Create sum.c

---
+++ secF/sum.c
@@ -0,0 +1,22 @@
+/*Write a program to calculate the sum of all positive integers
+1+2+3+4+....+10000
+
+*/
+
+#include<stdio.h>
+int main(){
+	int counter=1;
+	int sum =0;
+	
+	while(counter <=10000){
+	
+		sum = sum + counter;
+		counter++;
+	
+	}
+	
+	printf("The sum is: %d\n",sum );
+	
+	return 0;
+	
+}
