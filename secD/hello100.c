73857f3ea03670981ff4ccdf5373e79f14eacc33
elnazd <elnaz.delpisheh@gmail.com>
2014-05-22 07:09:39 -0400

    Create hello100.c

---
+++ secD/hello100.c
@@ -0,0 +1,16 @@
+/*Write a program to print 100 ‘Hello World!’ on the screen. 
+Each ‘Hello World!’ must start from a new line.
+*/
+#include<stdio.h>
+int main(){
+	int counter = 0;
+	
+	while(counter < 100){
+		printf("Hello world!\n");
+		counter++;
+	}
+	
+	
+	return 0;
+	
+}
