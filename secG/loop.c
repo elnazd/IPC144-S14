5d84de668f4e43182f67e102d278dc1217723531
Olivier St-Cyr <olivier.st-cyr@matrix.senecac.on.ca>
2014-05-23 09:56:11 -0400

    week3

--- secG/loop.c
+++ secG/loop.c
@@ -2,94 +2,38 @@
 
 int main()
 {
-    int menuSelection;
-    printf("Welcome to Seneca Bank\n");
-    printf("======================\n");
-    printf("Select one of the following option:\n\n");
-    printf("1 - Withdrawal\n");
-    printf("2 - Deposit\n");
-    printf("3 - Transfer\n");
-    printf("4 - Others\n");
-    printf("5 - Exit\n\n");
-    printf("Enter your selection: ");
-    scanf("%d", &menuSelection);
+    int sum = 0;
+    int mark;
+    int n = 0;
+    float average;
 
-    switch(menuSelection)
+    printf("Enter a mark (enter -1 to quit): ");
+    scanf("%d", &mark);
+
+    while(mark != -1)
     {
-        case 1:
-            printf("\nYou have selected withdrawal\n");
-            break;
-
-        case 2:
-            printf("\nYou have selected deposit\n");
-            break;
-
-        case 3:
-            printf("\nYou have selected transfer\n");
-            break;
-
-        case 4:
-            printf("\nYou have selected others\n");
-            break;
-
-        case 5:
-            printf("\nThank you, goodbye\n");
-            break;
-
-        default:
-            printf("\nInvalid selection\n");
-            break;
+        if(mark >= 0 && mark <=100)
+        {
+            sum += mark;  // sum = sum + mark;
+            n++;
+        }
+        else
+        {
+            printf("%d is an invalid input. Try again.\n\n", mark);
+        }
+        printf("Enter a mark (enter -1 to quit): ");
+        scanf("%d", &mark);
     }
 
-    /*if(menuSelection == 1)
+    if(n > 0)
     {
-        printf("\nYou have selected withdrawal\n");
-    }
-    else if(menuSelection == 2)
-    {
-        printf("\nYou have selected deposit\n");
-    }
-    else if(menuSelection == 3)
-    {
-        printf("\nYou have selected transfer\n");
-    }
-    else if(menuSelection == 4)
-    {
-        printf("\nYou have selected others\n");
-    }
-    else if(menuSelection == 5)
-    {
-        printf("\nThank you, goodbye\n");
+        average = (float)sum / n;
+        printf("The average is: %.2f \n", average);
     }
     else
     {
-        printf("\nInvalid selection\n");
-    }*/
+        printf("Cannot calculate average!\n");
+    }
 
     return 0;
 }
-
-
-/*
-switch(menuSelection)
-    {
-        case 1:
-            printf("\nYou have selected withdrawal\n");
-            break;
-        case 2:
-            printf("\nYou have selected deposit\n");
-            break;
-        case 3:
-            printf("\nYou have selected transfer\n");
-            break;
-        case 4:
-            printf("\nYou have selected others\n");
-            break;
-        case 5:
-            printf("\nThank you, goodbye\n");
-            break;
-        default:
-            printf("\nInvalid selection\n");
-            break;
-    }
-*/
