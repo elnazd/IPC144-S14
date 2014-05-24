b81155b9be4ff5907ea0cee12bbc810a1d5a57a9
elnazd <elnaz.delpisheh@gmail.com>
2014-05-24 15:55:10 -0400

    Create backwardsFor.c

---
+++ secF/backwardsFor.c
@@ -0,0 +1,12 @@
+//Print all positive numbers less  than 100 backwards!
+#include<stdio.h>
+int main(){
+
+	int counter;
+	
+	for(counter=100;counter>=1;counter--)
+		printf("%d,",counter);
+
+	return 0;
+
+}
