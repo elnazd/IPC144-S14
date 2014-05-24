6eed7d530b285e6c1e9dd0c25b66186f5d198d65
elnazd <elnaz.delpisheh@gmail.com>
2014-05-24 15:58:13 -0400

    Create flagV2.c

---
+++ secF/flagV2.c
@@ -0,0 +1,20 @@
+/*Write a program to input 10 integers from the user. 
+The user must input ‘0’ to exit.*/
+
+#include<stdio.h>
+int main(){
+
+	int num,counter=0;
+	
+	do{
+		printf("Enter an integer:\n");
+		scanf("%d",&num);
+		counter++;
+		
+	}while(counter<10 && num !=0);
+
+	
+
+
+	return 0;
+}
