a5672a95cf9e89a521f8f77f91d46b7c546a9a09
elnazd <elnaz.delpisheh@gmail.com>
2014-05-24 16:08:41 -0400

    Create harmonic.c

---
+++ secD/harmonic.c
@@ -0,0 +1,18 @@
+/*1/1 + 1/2 + 1/3 +...+ 1/10000*/
+#include<stdio.h>
+int main(){
+
+	int counter=1;
+	float sum=0.0;
+	while(counter<10000){
+	
+		sum=sum+ (1.0/counter); 
+		counter++;
+	
+	}
+
+	printf("The sum is: %.2f",sum);
+
+	return 0;
+
+}
