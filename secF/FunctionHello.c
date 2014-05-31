75c47cf691a8d2eb5ea4b19ff76fe4cf1f68b208
elnazd <elnaz.delpisheh@gmail.com>
2014-05-31 13:27:03 -0400

    Create FunctionHello.c

---
+++ secF/FunctionHello.c
@@ -0,0 +1,20 @@
+
+/*Write a function to print “Hello world!”. 
+Call that function from the main program.
+*/
+
+#include<stdio.h>
+
+void hello();
+int main(){
+
+	//Calling hello():
+	hello();
+	return 0;
+}
+
+void hello(){
+
+	printf("Hello World!");
+
+}
