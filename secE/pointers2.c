0059522cf5eae3615690ef5022c6afd90a347f88
elnazd <elnaz.delpisheh@gmail.com>
2014-06-03 19:37:53 -0400

    Create pointers2.c

---
+++ secE/pointers2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main ()
+{
+   int  var = 20;   /* actual variable declaration */
+   int  *ip = &var;  /* store address of var in pointer variable*/
+
+   printf("Address of var variable: %x\n", &var  );
+
+   printf("Value of *ip variable: %d\n", *ip );
+   
+   *ip = 10;
+   
+   printf("Address of var variable: %x\n", &var  );
+
+  printf("Value of var variable: %d\n", var );
+  
+   printf("Value of *ip variable: %d\n", *ip );
+   
+
+   return 0;
+}
