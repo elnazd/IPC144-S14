3b63054085c28868c22ada3b9399b490b6683a2f
elnazd <elnaz.delpisheh@gmail.com>
2014-06-10 12:47:33 -0400

    Create nestedLoop.c

---
+++ secD/nestedLoop.c
@@ -0,0 +1,21 @@
+#define maxRow 4
+#define maxCol 4
+
+#include<stdio.h>
+int main () 
+{
+    int row,col;
+    for (row=1; row<=maxRow; row++)
+    {
+        for (col=1; col<=maxCol; col++)
+        {
+			if(col < row)
+				printf (" %d", row%col);
+			else
+				printf(" %d", row*col);
+		}
+        printf ("\n");
+    }
+   return 0;
+}
+
