59c9291910e1f7c9c361ed39c6dd658fdd0a59c6
elnazd <elnaz.delpisheh@gmail.com>
2014-06-23 21:52:07 -0400

    Create sqrt.c

---
+++ secD/sqrt.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int number,answer;
+	printf("Please enter an integer:\n");
+	scanf("%d", &number);
+	
+	answer = sqrt(number);
+	printf("%d", answer); 
+	
+
+	return 0;
+}
