0cf2e7a492fd324238a7e2ba93c1df6eaf6fed04
elnazd <elnaz.delpisheh@gmail.com>
2014-07-18 23:09:22 -0400

    Create listofNames.c

---
+++ secF/listofNames.c
@@ -0,0 +1,15 @@
+/*Ann
+James
+Bill
+Jim*/
+#include<stdio.h>
+int main()
+{
+	int i;
+	char names[4][31]={"Ann","James","Bill","Jim"};
+	//Display the names:
+	for(i=0;i<4;i++)
+		printf("%s\n",names[i]);
+
+
+}
