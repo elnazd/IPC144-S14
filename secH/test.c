e43d8b3ae43475dcf96ac94b80f83050c4737164
Parul Kantaria <parul.kantaria@matrix.senecac.on.ca>
2014-08-05 08:57:03 -0400

    file

--- secH/test.c
+++ secH/test.c
@@ -1,31 +1,37 @@
-#include<stdio.h>
-struct student
+#include <stdio.h>
+struct student 
 {
-char n[30];  
-int id;
-  float marks;
-  
+	char name[30];
+	int id;
+	float mark;
 };
 int main(void)
 {
-   int i,n;
-   struct student s[3];
-   for(i=0;i<3;i++)
-   {
-     
-     printf("enter name=");
-     //gets(s[i].name);
-     fgets(s[i].n,30,stdin);
-     printf("enter id=");
-     scanf("%d",&s[i].id);
-     printf("enter marks=");
-     scanf("%f",&s[i].marks);
-
-   }
-   for(i=0;i<3;i++)
-   {
-      printf("%s\t%d\t%f\n",s[i].n,s[i].id,s[i].marks);
-   } 
-return 0;
+	FILE *fp = NULL;
+	
+	struct student s;
+    printf("enter student name=");
+	scanf("%s",s.name);
+	printf("enter student id=");
+	scanf("%d",&s.id);
+	printf("enter student mark=");
+	scanf("%f",&s.mark);     	
+    fp = fopen("d:\\abc.txt","a");     
+         if (fp != NULL) {
+                 fprintf(fp,"%s %d %f",s.name,s.id,s.mark);
+                 fclose(fp);
+         } else {
+                 printf("Failed to open file\n");
+         }
+         
+         fp = fopen("d:\\abc.txt","r");
+         if (fp != NULL) {
+                 while(fscanf(fp,"%s %d %f",s.name,&s.id,&s.mark)!=EOF)
+                         printf("You read : %s  %d   %f\n",s.name,s.id,s.mark);
+                 fclose(fp);
+         } else {
+                 printf("Failed to open file\n");
+         }
+    return 0;
 }
 
